86a597ab7b79a23aeccc584067b9cbd291894162
shuoxu <shuoxu@Ctrip.com>
2018-08-23 10:21:30 +0800

    input file

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ const FILENAME = 'input.xlsx';
 
 var obj = xlsx.parse(__dirname+'/'+FILENAME);//配置excel文件的路径
 
-var excelObj=obj[7].data;//excelObj是excel文件里第一个sheet文档的数据，obj[i].data表示excel文件第i+1个sheet文档的全部内容
+// obj.map((v,k)=>{
+// 	console.log(v.name,k)
+// })
+
+//console.log(obj[6].data[3],obj[6].data[4])
+var a = obj[6].data[3];
+
+
+console.log(a[14],a[15],a[16])
+
+
+var gll7 = obj[6].data;
+var fxt7 = obj[7].data;
+
+
+
+//excelObj是excel文件里第一个sheet文档的数据，obj[i].data表示excel文件第i+1个sheet文档的全部内容
  //console.log(excelObj[0],excelObj[1]);
 //一个sheet文档中的内容包含sheet表头 一个excelObj表示一个二维数组，excelObj[i]表示sheet文档中第i+1行的数据集（一行的数据也是数组形式，访问从索引0开始）
 
@@ -37,55 +53,84 @@ var getS = function(date){
 
 
 //return
-var a = new Date(1900,0,excelObj[3][12]).toLocaleString();
-var b = new Date(1900,0,excelObj[3][15]).toLocaleString();
-//console.log(a,b)
-
-var ca = new Date(b).getTime()-new Date(a).getTime();
+var aaa = new Date(1900,0,a[15]).toLocaleString();
+//var bbb = new Date(a[16]).getTime();
+var ccc = new Date(aaa).getTime()
+console.log(ccc,'qqqqqqqqq')
+//return
+//var ca = new Date(b).getTime()-new Date(a).getTime();
 // console.log(ca)
 // console.log(ca/minute);
 // console.log(excelObj[4][15])
-var da = new Date(1900,0,excelObj[4][15]).toLocaleString();
+//var da = new Date(1900,0,excelObj[4][15]).toLocaleString();
 // console.log(da)
 // console.log(new Date(da).getMinutes());
 // console.log(new Date(da).getSeconds());
 
 
 
-var result = [];
-result[0] = excelObj[0]
-excelObj.map(function(v,k){
-	if(k>0){
+function main(excelObj){
+	var resultwp1d = [];
+	resultwp1d[0] = excelObj[0];
+	resultwp1d[1] = excelObj[1];
+	resultwp1d[2] = excelObj[2];
 
-		//console.log(v[12],v[15])
-		var wechat = new Date(1900,0,v[12]).toLocaleString();
-		var pos = new Date(1900,0,v[15]).toLocaleString();
-	//console.log(a,b)
-		if((new Date(wechat)).getMinutes()!=0 || (new Date(wechat)).getSeconds()!=0){
-			//var ca =Math.abs(new Date(pos).getTime()-new Date(wechat).getTime());
-			var ca =new Date(pos).getTime()-new Date(wechat).getTime();
-			//console.log(ca)
-			if(isNaN(ca)){
-				console.log(ca)
-				result.push(v)
-			}else{
-				if(ca/minute<=30){
-					console.log(v[1],ca/minute)
-					result.push(v)
-				}
-			}
-			// if(ca/minute<=30){
-			// 	//console.log(v[1],ca/minute)
-			// }
-		}else{
-			//console.log(v[1])
-		}
-	}
+	var resultpa1d = [...resultwp1d];
+	var resultwp2h = [...resultwp1d];
+	var resultpa2h = [...resultwp1d];
+	excelObj.map(function(v,k){
+		if(k>2){
 	
-})
+			//console.log(v[12],v[15])
+			var wechat = new Date(1900,0,v[14]).toLocaleString();
+			var pos = new Date(1900,0,v[15]).toLocaleString();
+			var app = new Date(v[16]).getTime();
+		//console.log(a,b)
+			//if((new Date(wechat)).getMinutes()!=0 || (new Date(wechat)).getSeconds()!=0){
+				//var ca =Math.abs(new Date(pos).getTime()-new Date(wechat).getTime());
+				var wp = Math.abs(new Date(wechat).getTime()-new Date(pos).getTime());
+				var pa = Math.abs(new Date(pos).getTime()-app);
+				//console.log(ca)
+				 
+				//console.log(wp/day,pa/day,wp/hour,pa/hour)
+				if(wp/day<=1){
+					//console.log(wp/day)
+					resultwp1d.push(v)
+				}
 
-console.log(result)
-return
+				if(pa/day<=1){
+					console.log(pa/day)
+					console.log(pos,v[16])
+					resultpa1d.push(v)
+				}
+
+				if(wp/hour<=2){
+					//console.log(wp/hour)
+					resultwp2h.push(v)
+				}
+
+				if(pa/hour<=2){
+					resultpa2h.push(v)
+				}
+				
+				
+				// if(ca/minute<=30){
+				// 	//console.log(v[1],ca/minute)
+				// }
+			// }else{
+			// 	//console.log(v[1])
+			// }
+		}
+		
+	})
+
+	return {resultwp1d,resultpa1d,resultwp2h,resultpa2h}
+}
+var _obj = main(fxt7)
+
+
+//console.log(result)
+//return
 
 
 
@@ -112,10 +157,23 @@ return
 // }
 var buffer = xlsx.build([
     {
-        name:'sheet1',
-        data:result
-    }        
+        name:'微信pos1天内',
+        data:_obj.resultwp1d
+	},
+	{
+        name:'posapp1天内',
+        data:_obj.resultpa1d
+	},
+	{
+        name:'微信pos2小时',
+        data:_obj.resultwp2h
+	},
+	{
+        name:'posapp2小时',
+        data:_obj.resultpa2h
+    }  
+	       
 ]);
 
 //将文件内容插入新的文件中
-fs.writeFileSync('test1.xlsx',buffer,{'flag':'w'});
+fs.writeFileSync('test2.xlsx',buffer,{'flag':'w'});
